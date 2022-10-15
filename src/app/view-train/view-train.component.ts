c0e5d13ca7f8f13cedfee6241a28aff330641e9f
Amalpm7 <amal.p@nestgroup.net>
2022-10-15 09:20:08 +0530

    binding

---
+++ src/app/view-train/view-train.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-train',
+  templateUrl: './view-train.component.html',
+  styleUrls: ['./view-train.component.css']
+})
+export class ViewTrainComponent implements OnInit {
+
+  constructor() { }
+  name="Amal"
+
+  ngOnInit(): void {
+  }
+
+}
